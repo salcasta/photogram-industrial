a6e0e32e9e091cd2dad5b7fe15b5cb182b37dc10
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-02-28 19:45:13 +0000

    set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
+  resources :users, only: :show
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
